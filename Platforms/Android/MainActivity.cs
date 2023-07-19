44958dc2a0e0c0096ddd9240db3b5f5598fbcc76
noobmx <roberto_1320114135@uptecamac.edu.mx>
2023-07-19 17:04:14 -0600

    Agregar archivos de proyecto.

---
+++ Platforms/Android/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Content;
+using Android.Nfc;
+using Android.OS;
+
+namespace NFCReader;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+
+[MetaData(NfcAdapter.ActionTechDiscovered, Resource = "@xml/nfc_tech_filter")]
+[IntentFilter(new[] { NfcAdapter.ActionTechDiscovered }, Categories = new[] { Intent.CategoryDefault }, DataMimeType = "text/plain")]
+[IntentFilter(new[] { NfcAdapter.ActionNdefDiscovered }, Categories = new[] { Intent.CategoryDefault }, DataMimeType = "text/plain")]
+[IntentFilter(new[] { NfcAdapter.ActionTagDiscovered }, Categories = new[] { Intent.CategoryDefault }, DataMimeType = "text/plain")]
+
+public class MainActivity : MauiAppCompatActivity
+{
+}
